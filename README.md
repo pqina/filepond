953dce51d44baf0069541cfa0d80883ca4fcd971
Rik Schennink <rikschennink@gmail.com>
2017-10-20 14:48:25 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# FilePond
+
+FilePond repository v0.1.0
+
+Promotional demo can be found at https://pqina.nl/filepond
+
+Working towards version v1.0.0
---
+++ filepond.js
@@ -0,0 +1,6 @@
+/*
+ * filepond v0.1.0 - FilePond: Where files go to stretch their bits
+ * Copyright (c) 2017 PQINA - https://pqina.nl/filepond
+ */
+
+/* Intentionally left blank */
