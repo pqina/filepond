16fb2c8511eb9719ffa941f5a0a9dcabe07c1779
Rik <rik@pqina.nl>
2025-12-16 14:28:50 +0100

    add locale names to source

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,25 +1,50 @@
-export { locale as cs_CZ } from './cs-cz.js';
+/** Arabic */
 export { locale as ar_AR } from './ar-ar.js';
+/** Bengali */
 export { locale as bn_BD } from './bn-bd.js';
+/** Czech */
+export { locale as cs_CZ } from './cs-cz.js';
+/** Danish */
 export { locale as da_DK } from './da-dk.js';
+/** German */
 export { locale as de_DE } from './de-de.js';
+/** Greek */
 export { locale as el_GR } from './el-gr.js';
+/** English */
 export { locale as en_GB } from './en-gb.js';
+/** Spanish */
 export { locale as es_ES } from './es-es.js';
+/** Finnish */
 export { locale as fi_FI } from './fi-fi.js';
+/** French */
 export { locale as fr_FR } from './fr-fr.js';
+/** Hindi */
 export { locale as hi_IN } from './hi-in.js';
+/** Hungarian */
 export { locale as hu_HU } from './hu-hu.js';
+/** Icelandic */
 export { locale as is_IS } from './is-is.js';
+/** Italian */
 export { locale as it_IT } from './it-it.js';
+/** Japanese */
 export { locale as ja_JP } from './ja-jp.js';
+/** Korean */
 export { locale as ko_KR } from './ko-kr.js';
+/** Norwegian */
 export { locale as nb_NO } from './nb-no.js';
+/** Dutch */
 export { locale as nl_NL } from './nl-nl.js';
+/** Polish */
 export { locale as pl_PL } from './pl-pl.js';
+/** Portuguese */
 export { locale as pt_PT } from './pt-pt.js';
+/** Swedish */
 export { locale as sv_SE } from './sv-se.js';
+/** Turkish */
 export { locale as tr_TR } from './tr-tr.js';
+/** Ukrainian */
 export { locale as uk_UA } from './uk-ua.js';
+/** Vietnamese */
 export { locale as vi_VN } from './vi-vn.js';
+/** Chinese */
 export { locale as zh_CN } from './zh-cn.js';
